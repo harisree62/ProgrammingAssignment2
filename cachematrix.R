5b48dfe35b7d8ebd751a9edb1f6764b5502f126e
harisree62 <kadapa.sreehari@gmail.com>
2018-11-11 17:32:49 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,54 @@
 ## aim is to writer two functions to inverse a matrix
 
-## makeCacheMatrix is a function we are creating to store the inverse of the input matrix
+## makeCacheMatrix is a function we are creating an object to store the inverse of the input matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        inversed<- NULL
+        set<- function(a){
+        x<<-a
+                inversed<<- NULL
+                
+     }
+        get <- function() x
+        setInverse <- function(inverse) inversed<<- inverse
+        getInverse <- function() inversed
+        list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
+
+## cacheSolve function is written to compute the inverse of the matrix returned by above function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inversed <- x$getInverse()
+        if (!is.null(inversed)){
+                message("retreiving inverse calculated")
+        return(inversed)
+        }
+matr<- x$get()
+inversed<- solve(matr, ...)
+x$setInverse(inversed)
+        inversed
 }
+
+
+
+## executed result
+##>matd<-makeCacheMatrix(matrix(1:4,2,2))
+## >matd$get()
+##     [,1] [,2]
+##[1,]    1    3
+##[2,]    2    4
+## >matd$getInverse()
+##NULL
+## >cacheSolve(matd)
+##     [,1] [,2]
+##[1,]   -2  1.5
+##[2,]    1 -0.5
+## >cacheSolve(matd)
+##retreiving inverse calculated
+##     [,1] [,2]
+##[1,]   -2  1.5
+##[2,]    1 -0.5
